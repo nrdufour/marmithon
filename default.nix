65be19959d6aca1f7eb5291e9ec1c686fc2d47dd
Nicolas Dufour <nrdufour@gmail.com>
2025-11-06 17:10:47 -0500

    feat(nix): simplify

--- default.nix
+++ default.nix
@@ -1,17 +1,10 @@
-{ pkgs ? import <nixpkgs> {
-    config.allowUnfree = true;
-  }
-, unstable ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
-    config.allowUnfree = true;
-  }
-}:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [
-      flyctl
-      go-task
-      unstable.claude-code
-      jq
-      sqlite
-    ];
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs.buildPackages; [
+    flyctl
+    go-task
+    jq
+    sqlite
+  ];
 }
